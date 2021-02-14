3b68dccc77e147ca08dbd4d12ce9dfb086857fdd
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:26:31 -0300

    Add files via upload

---
+++ Uri 1007/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uri_1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, diferenca;
+
+            Console.WriteLine();
+            Console.Write("Digite o primeiro número : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o Segundo número : ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o Terceiro número : ");
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o Quarto número : ");
+            d = int.Parse(Console.ReadLine());
+
+            diferenca = (a * b) - (c * d);
+
+            Console.WriteLine();
+            Console.Write("Diferença = " +diferenca);
+        }
+    }
+}
